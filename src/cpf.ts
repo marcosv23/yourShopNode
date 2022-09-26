335dacf3cc991f3be2215fdb5996359eaa23b20a
marcos.moraes <marcos.moraes@luizalabs.com>
2022-09-25 23:04:25 -0300

    fix: refactoring cpf

--- src/cpf.ts
+++ src/cpf.ts
@@ -1,45 +1,41 @@
 export class Cpf{
 
-public static validate (str:string) {
-	if (!str) return false;
-    if (str.length < 11 || str.length > 14) return false;
-        str=str
+public static validate (cpf:string) {
+	if (!cpf) return false;
+    cpf = Cpf.cleanCpf(cpf);
+    if (cpf.length !== 11) return false;
+    const hasAllSameDigits = cpf.split("").every(c => c === cpf[0]);        
+    if (hasAllSameDigits)  return false
+            let  digit1, digit2;  
+            let  verifierDigit1, verifierDigit2, divisionRest;  
+            let  currentPosition;  
+            let  nDigResult;  
+            digit1 = digit2 = verifierDigit1 = verifierDigit2 = divisionRest = 0;   
+            for (let index = 1; index < cpf.length -1; index++) {  
+                currentPosition = parseInt(cpf.substring(index -1, index));  							
+                digit1 = digit1 + ( 11 - index ) * currentPosition;  
+                digit2 = digit2 + ( 12 - index ) * currentPosition;
+            };   
+            divisionRest = (digit1 % 11);  
+            verifierDigit1 = (divisionRest < 2) ? verifierDigit1 = 0 : 11 - divisionRest;  
+            digit2 += 2 * verifierDigit1;  
+            divisionRest = (digit2 % 11);  
+            if (divisionRest < 2)  
+                verifierDigit2 = 0;  
+            else
+                verifierDigit2 = 11 - divisionRest;  
+                let nDigVerific = cpf.substring(cpf.length-2, cpf.length);  
+            nDigResult = "" + verifierDigit1 + "" + verifierDigit2;  
+            return nDigVerific == nDigResult;  
+    
+}
+
+    private static cleanCpf(cpf:string){
+        return cpf
             .replace('.','')
             .replace('/','')
             .replace('.','')
             .replace('-','')
-            .replace(" ","");  
-    if (!str.split("").every(c => c === str[0])) {
-        try{  
-            let  digit1, digit2;  
-            let  dg1, dg2, divisionRest;  
-            let  currentPosition;  
-            let  nDigResult;  
-            digit1 = digit2 = 0;  
-            dg1 = dg2 = divisionRest = 0;  
-                
-            for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                currentPosition = parseInt(str.substring(nCount -1, nCount));  							
-                digit1 = digit1 + ( 11 - nCount ) * currentPosition;  
-                digit2 = digit2 + ( 12 - nCount ) * currentPosition;
-            };   
-            divisionRest = (digit1 % 11);  
-            dg1 = (divisionRest < 2) ? dg1 = 0 : 11 - divisionRest;  
-            digit2 += 2 * dg1;  
-            divisionRest = (digit2 % 11);  
-            if (divisionRest < 2)  
-                dg2 = 0;  
-            else  
-                dg2 = 11 - divisionRest;  
-    
-                let nDigVerific = str.substring(str.length-2, str.length);  
-            nDigResult = "" + dg1 + "" + dg2;  
-            return nDigVerific == nDigResult;
-        }catch (e){  
-            console.error("Erro !"+e);  
-
-            return false;  
-        }  
-    } else return false
-}
+            .replace(" ","");
+    }
 }
--- test/cpf/cpf.test.ts
+++ test/cpf/cpf.test.ts
@@ -1,24 +1,40 @@
 import { Cpf } from "../../src/cpf"
 
 describe("CPF TESTS",()=>{
+
+    test("Should not validate when has not a cpf",()=>{
+        expect(Cpf.validate("")).toBeFalsy();
+    })
+
+    test("Should not validate when has a cpf less than 11 chars",()=>{
+        expect(Cpf.validate("12345")).toBeFalsy();
+    })
+
+    test("Should not validate when has a cpf more than 14 chars",()=>{
+        expect(Cpf.validate("151.673.500---50")).toBeFalsy();
+    })
     test("Should return true for valid cpf",()=>{
-    expect(Cpf.validate("151.673.500-50")).toBe(true);
+    expect(Cpf.validate("151.673.500-50")).toBeTruthy();
     })
     test("Should return true for valid cpf without mask",()=>{
-        expect(Cpf.validate("15167350050")).toBe(true);
+        expect(Cpf.validate("15167350050")).toBeTruthy();
     })
     test("Should return true for valid cpf with invalid mask",()=>{
-         expect(Cpf.validate("151673500/50")).toBe(true);
+         expect(Cpf.validate("151673500/50")).toBeTruthy();
     })
     test("Should return false for invalid cpf",()=>{
-        expect(Cpf.validate("15167350051")).toBe(false);
-   })
+        expect(Cpf.validate("15167350051")).toBeFalsy();
+    })
+
+    test("Should return true for valid cpf with zero on first digit",()=>{
+        expect(Cpf.validate("198.454.187-08")).toBeTruthy();
+    })
    
    test("Should return false for repeated digits",()=>{
-    expect(Cpf.validate("11111111111")).toBe(false);
+    expect(Cpf.validate("11111111111")).toBeFalsy();
     })
 
     test("Should return false for repeated digits with mask",()=>{
-        expect(Cpf.validate("111.111.111-11")).toBe(false);
+        expect(Cpf.validate("111.111.111-11")).toBeFalsy();
     })
 })
